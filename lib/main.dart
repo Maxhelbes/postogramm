0e16f4ea9e6462faf78adbfe7d753798c7bfb159
Maxhelbes <Maxhelbes@yandex.ru>
2025-03-12 18:17:59 +0300

    create base UI of GridScreen

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,46 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(home: PhotoScreen());
+  }
+}
+
+class GridScreen extends StatelessWidget {
+  const GridScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Постограм'),
+        centerTitle: true,
+        actions: [Icon(Icons.more_vert)],
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10.0),
+          child: GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 4,
+              crossAxisSpacing: 3,
+              mainAxisSpacing: 5,
+            ),
+            itemCount: 10,
+            itemBuilder: (context, index) {
+              return Placeholder();
+            },
+          ),
         ),
       ),
     );
+  }
+}
+
+class PhotoScreen extends StatelessWidget {
+  const PhotoScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: SafeArea(child: Placeholder()));
   }
 }
